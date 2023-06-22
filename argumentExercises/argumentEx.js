3f277f125343d6851d1ac4346843f818f5827ec1
Jordy <jordycorporan95@gmail.com>
2023-06-22 16:53:34 -0400

    Finished Part 1

--- argumentExercises/argumentEx.js
+++ argumentExercises/argumentEx.js
@@ -43,41 +43,41 @@ class Cat {
     constructor(name) {
       this.name = name;
     }
-  
+
     says(sound, person) {
       console.log(`${this.name} says ${sound} to ${person}!`);
       return true;
     }
   }
-  
+
   class Dog {
     constructor(name) {
       this.name = name;
     }
   }
-  
+
   const markov = new Cat("Markov");
   const pavlov = new Dog("Pavlov");
-  
+
   markov.says("meow", "Ned");
   // Markov says meow to Ned!
   // true
-  
+
   // bind time args are "meow" and "Kush", no call time args
   markov.says.myBind(pavlov, "meow", "Kush")();
   // Pavlov says meow to Kush!
   // true
-  
+
   // no bind time args (other than context), call time args are "meow" and "a tree"
   markov.says.myBind(pavlov)("meow", "a tree");
   // Pavlov says meow to a tree!
   // true
-  
+
   // bind time arg is "meow", call time arg is "Markov"
   markov.says.myBind(pavlov, "meow")("Markov");
   // Pavlov says meow to Markov!
   // true
-  
+
   // no bind time args (other than context), call time args are "meow" and "me"
   const notMarkovSays = markov.says.myBind(pavlov);
   notMarkovSays("meow", "me");
@@ -113,7 +113,7 @@ class Cat {
 //     }
 // }
 
-//Why doesn't this work
+
 function curriedSum(numArgs) {
     const nums = [];
     return function _curriedSum(num) {
@@ -126,7 +126,23 @@ function curriedSum(numArgs) {
     }
 }
 
+function sumThree(num1, num2, num3) {
+    return num1 + num2 + num3;
+  }
 
 
-
-  
+Function.prototype.curry = function(numArgs) {
+    debugger
+    originalFunc = this;
+    const nums = [];
+    return function _curried(num) {
+        nums.push(num);
+        debugger
+        if(nums.length === numArgs) {
+            return originalFunc.call(null, ...nums);
+        }
+        else{
+            return _curried;
+        }
+    }
+}
